3de13366bfc49e8f8961a6d09318fa7d666cf0a8
cainamisir <vlad.cainamisir@gmail.com>
2021-08-07 16:22:30 +0300

    Cart v0

--- methods/user.js
+++ methods/user.js
@@ -1,114 +1,151 @@
 const sha256 = require("js-sha256");
 const jwt = require("jsonwebtoken");
 const userUtils = require("./userUtilsService");
-
 function createUser(model, req, res) {
-  let user = new model({ ...req.body, password: sha256(req.body.password) });
-  user.save((err, data) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send({ error: err });
-    } else {
-      console.log(data);
-      res.status(200).send({ data: "User created successfully" });
-    }
-  });
-}
-
-function loginUser(model, req, res) {
-  let email = req.body.email;
-  let password = req.body.password;
-  model
-    .findOne({ email: email, password: sha256(password) })
-    .then((data, err) => {
-      if (data && !err) {
-        let isAdmin = false;
-        if (
-          email == "mihai.indreias@gmail.com" ||
-          email == "vlad.cainamisir@gmail.com"
-        )
-          isAdmin = true;
-        const token = jwt.sign(
-          {
-            user_id: data._id,
-            email: email,
-            verified: data.verified,
-            password: sha256(password),
-            isAdmin: isAdmin,
-          },
-          process.env.TOKEN_KEY,
-          {
-            expiresIn: "12h",
-          }
-        );
-        console.log(token);
-        res.status(200).send({ token: token });
-      } else {
-        res
-          .status(500)
-          .send({ error: "Could not find this user/password combination" });
-      }
+    let user = new model({ ...req.body, password: sha256(req.body.password) });
+    user.save((err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send({ error: err });
+        } else {
+            console.log(data);
+            res.status(200).send({ data: "User created successfully" });
+        }
     });
 }
 
-function getPersonalInfo(model, req, res) {
-  let userToken = req.params.token;
-  try {
-    let decoded = jwt.verify(userToken, process.env.TOKEN_KEY);
+function loginUser(model, req, res) {
+    let email = req.body.email;
+    let password = req.body.password;
     model
-      .findOne({
-        _id: decoded.user_id,
-        email: decoded.email,
-        password: decoded.password,
-      })
-      .then((data, err) => {
-        if (data && !err) {
-          console.log(data);
-          res.status(200).send(data);
-        } else
-          res
-            .status(404)
-            .send({ error: "Could not find user with this id/email" });
-      });
-  } catch (e) {
-    console.log(e);
-    res.status(404).send({ error: e });
-  }
+        .findOne({ email: email, password: sha256(password) })
+        .then((data, err) => {
+            if (data && !err) {
+                let isAdmin = false;
+                if (
+                    email == "mihai.indreias@gmail.com" ||
+                    email == "vlad.cainamisir@gmail.com"
+                )
+                    isAdmin = true;
+                const token = jwt.sign(
+                    {
+                        user_id: data._id,
+                        email: email,
+                        verified: data.verified,
+                        password: sha256(password),
+                        isAdmin: isAdmin,
+                    },
+                    process.env.TOKEN_KEY,
+                    {
+                        expiresIn: "12h",
+                    }
+                );
+                console.log(token);
+                res.status(200).send({ token: token });
+            } else {
+                res.status(500).send({
+                    error: "Could not find this user/password combination",
+                });
+            }
+        });
+}
+
+function getPersonalInfo(model, req, res) {
+    let userToken = req.params.token;
+    try {
+        let decoded = jwt.verify(userToken, process.env.TOKEN_KEY);
+        model
+            .findOne({
+                _id: decoded.user_id,
+                email: decoded.email,
+                password: decoded.password,
+            })
+            .then((data, err) => {
+                if (data && !err) {
+                    console.log(data);
+                    res.status(200).send(data);
+                } else
+                    res.status(404).send({
+                        error: "Could not find user with this id/email",
+                    });
+            });
+    } catch (e) {
+        console.log(e);
+        res.status(404).send({ error: e });
+    }
 }
 
 function updatePersonalInformation(model, req, res) {
-  let userToken = req.body.token;
-  try {
-    let decoded = jwt.verify(userToken, process.env.TOKEN_KEY);
-    let newUserInfo = req.body.userInfo;
-    let userDocument = new model(newUserInfo);
-    console.log(newUserInfo, userDocument);
-    delete newUserInfo.verified;
-    delete newUserInfo._id;
-    delete newUserInfo.password;
-    model.findOneAndUpdate(
-      {
-        _id: decoded.user_id,
-        email: decoded.email,
-        password: decoded.password,
-      },
-      newUserInfo,
-      (err, data) => {
-        if (err || !data)
-          console.log(err), res.status(500).send({ error: err });
-        else
-          res
-            .status(200)
-            .send({ response: "Successfully updated user information" });
-      }
-    );
-  } catch (e) {
-    console.log(e);
-    res.status(404).send({ error: e });
-  }
+    let userToken = req.body.token;
+    try {
+        let decoded = jwt.verify(userToken, process.env.TOKEN_KEY);
+        let newUserInfo = req.body.userInfo;
+        let userDocument = new model(newUserInfo);
+        console.log(newUserInfo, userDocument);
+        delete newUserInfo.verified;
+        delete newUserInfo._id;
+        delete newUserInfo.password;
+        model.findOneAndUpdate(
+            {
+                _id: decoded.user_id,
+                email: decoded.email,
+                password: decoded.password,
+            },
+            newUserInfo,
+            (err, data) => {
+                if (err || !data)
+                    console.log(err), res.status(500).send({ error: err });
+                else
+                    res.status(200).send({
+                        response: "Successfully updated user information",
+                    });
+            }
+        );
+    } catch (e) {
+        console.log(e);
+        res.status(404).send({ error: e });
+    }
+}
+
+function addToCart(userModel, productModel, req, res) {
+    let userToken = req.body.token;
+    let productID = req.body.productId;
+    let quantity = req.body.quantity;
+    productModel.findOne({ _id: productID }, (err, data) => {
+        console.log(data, err);
+        if (err || !data) {
+            res.status(400).send({ error: "Invalid product id" });
+        } else {
+            try {
+                let decoded = jwt.verify(userToken, process.env.TOKEN_KEY);
+                userModel.findOneAndUpdate(
+                    {
+                        _id: decoded.user_id,
+                        email: decoded.email,
+                        password: decoded.password,
+                    },
+                    { $push: { Cart: { productID, quantity } } },
+                    (err, data) => {
+                        if (err || !data) {
+                            console.log(err),
+                                res.status(500).send({ error: err });
+                        } else {
+                            res.status(200).send({
+                                response: "Item added to cart",
+                            });
+                        }
+                    }
+                );
+            } catch (e) {
+                console.log(e);
+                res.status(500).send({ error: e });
+            }
+        }
+    });
 }
 
 exports.createUser = createUser;
 exports.loginUser = loginUser;
 exports.getPersonalInfo = getPersonalInfo;
 exports.updatePersonalInformation = updatePersonalInformation;
+exports.addToCart = addToCart;
